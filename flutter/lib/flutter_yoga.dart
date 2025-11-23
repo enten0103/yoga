4e667f5c2aff7ed6d8ef6cd85d40d2400506404f
enten0103 <enten@gmail.com>
2025-11-23 21:27:24 +0800

    fix:暴露更多数据结构

--- flutter/lib/flutter_yoga.dart
+++ flutter/lib/flutter_yoga.dart
@@ -2,6 +2,9 @@ import 'flutter_yoga_platform_interface.dart';
 
 export 'src/yoga_ffi.dart';
 export 'src/yoga_node.dart';
+export 'src/yoga_value.dart';
+export 'src/yoga_border.dart';
+export 'src/yoga_background.dart';
 export 'src/widgets/yoga_layout.dart';
 export 'src/rendering/yoga_layout.dart';
 
